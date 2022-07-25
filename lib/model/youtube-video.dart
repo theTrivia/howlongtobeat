de40138860502f87c1eccbef7d9e3ebde1bda3ce
theTrivia <bfmvsp@gmail.com>
2022-07-25 20:45:58 +0530

    added support for youtube video url generation

---
+++ lib/model/youtube-video.dart
@@ -0,0 +1,26 @@
+class YoutubeVideo {
+  final channelId;
+  final channelTitle;
+  final channelUrl;
+  final description;
+  final duration;
+  final id;
+  final kind;
+  final publishedAt;
+  final thumbnail;
+  final title;
+  final url;
+
+  YoutubeVideo(
+      this.channelId,
+      this.channelTitle,
+      this.channelUrl,
+      this.description,
+      this.duration,
+      this.id,
+      this.kind,
+      this.publishedAt,
+      this.thumbnail,
+      this.title,
+      this.url);
+}
